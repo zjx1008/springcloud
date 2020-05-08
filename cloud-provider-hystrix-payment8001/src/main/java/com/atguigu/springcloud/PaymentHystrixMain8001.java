06c03183c839dc32f3a506da3ff6583eb2f66201
zjx1008 <13141064938@163.com>
2020-05-08 16:47:17 +0800

    添加服务降级的注解 @HystrixCommand fallbackMethod 属性表示 如果需要服务降级兜底的方法是那个  commandProperties 属性可是设置多个属性  @HystrixProperty注解表示可以设置哪些属性  execution.isolation.thread.timeoutInMilliseconds   表示设置超时时间
     再主启动类 上添加@EnableCircuitBreaker 用来开启服务降级功能

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zjx
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.service;
 
 import ch.qos.logback.core.util.TimeUtil;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.micrometer.core.instrument.util.TimeUtils;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,21 @@ public class PaymentService {
     }
 
 
+    /***
+     * @HystrixCommand 注解可以设置服务降级，
+     *              fallbackMethod 属性表示 如果需要服务降级兜底的方法是那个
+     *              commandProperties 属性可是设置多个属性
+     *              @HystrixProperty注解表示可以设置哪些属性
+     *                  execution.isolation.thread.timeoutInMilliseconds   表示设置超时时间
+     *
+     *
+     */
+    @HystrixCommand(fallbackMethod = "Payment_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value ="3000" )
+    })
     public String Payment_TimeOut(Integer id) {
-        int time = 3;
+        int result =10/0;
+        int time = 5;
         try {
             TimeUnit.SECONDS.sleep(time);
         } catch (InterruptedException e) {
@@ -29,5 +44,11 @@ public class PaymentService {
         }
         return "当前线程" + Thread.currentThread().getName() + "Payment_TimeOut启动成功, \t" + id + "哈哈 耗时（秒）：" + time;
 
+    }
+
+    public String Payment_TimeOutHandler(Integer id){
+
+
+        return  "当前线程"+Thread.currentThread().getName()+"Payment_TimeOut方法异常，请稍后再试,id="+id+"\t ( ▼-▼ )";
     }
 }
