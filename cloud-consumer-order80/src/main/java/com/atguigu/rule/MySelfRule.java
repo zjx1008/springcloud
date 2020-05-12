a5289814fbec30c21917c0428a6114e53fe510fb
zjx1008 <13141064938@163.com>
2020-05-12 10:17:24 +0800

    设置Ribbon 的负载均衡，修改默认轮询方式 ，返回IRule， 随机方式 注意： 该类不能再主启动类的同级或子目录下，只能另外开一个新不能被它扫描到的目录，要不然 会设置为所有微服务全局使用 再启动类上添加@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class) name表示 需要使用的微服务名称 configuration 表示 使用该负载均衡

--- cloud-consumer-order80/src/main/java/com/atguigu/rule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/rule/MySelfRule.java
@@ -10,6 +10,12 @@ import org.springframework.context.annotation.Configuration;
  * @create 2020-05-07-14:21
  */
 
+/***
+ * 设置Ribbon 的负载均衡，修改默认轮询方式 ，返回IRule， 随机方式
+ * 注意： 该类不能再主启动类的同级或子目录下，只能另外开一个新不能被它扫描到的目录，要不然 会设置为所有微服务全局使用
+ * 再启动类上添加@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
+ * name表示 需要使用的微服务名称 configuration 表示 使用该负载均衡
+ */
 @Configuration
 public class MySelfRule {
     @Bean
