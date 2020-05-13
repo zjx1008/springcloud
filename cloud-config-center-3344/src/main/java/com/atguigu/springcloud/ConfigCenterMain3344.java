b4e5cc1fee946aeca7c03a8b66037cebda0d3277
zjx1008 <13141064938@163.com>
2020-05-13 16:56:51 +0800

    这个是spring cloud config  的服务端模块
    springcloud config    是为微服务架构中的微服务提供了外部配置支持，配置服务器位各不相同微服务应用的所有环境提供了一个中心化的外部配置
    第一步 引入在pom 文件里引入springcloud config  的maven 坐标
    
    <dependency>
        <groupId>org.springframework.cloud</groupId>
        <artifactId>spring-cloud-config-server</artifactId>
    </dependency>
    
    第二步 编写yml文件，把当前服务注册到 eureka， 然后添加spring cloud  的相关配置
    
    server:
      port: 3344
    spring:
      application:
        name: cloud-config-center
      cloud:
        config:
          server:
            git:
    #          uri: https://github.com/zzyybs/springcloud-config.git   这个是周阳老是的
              uri: https://github.com/zjx1008/springcloud-config.git   #这个是我自己的，有一个问题是周阳老是的可以直接使用git@github.com 这样的方式 访问，但是我用这种方式直接报错
              search-paths:                                            #这个是需要访问的目录 可能会用多个 所有 使用 -
               - springcloud-config
          label: master                                                #这个是需要访问的分支
    
    eureka:
      client:
        register-with-eureka: true
        fetch-registry: true
        service-url:
          defaultZone: http://localhost:7001/eureka
    
    yml读取文件规则
      /{label}/{application}-{profile}.yml     例如：master/config-dev.yml
      /{applicatoin}-{profile}.yml                 例如： /config-dev.yml   默认读取master分支的文件
     /{application}/{profile}/{label}              例如： /config/dev/master 意思是 读取 master 分支下的config-dev.yml 文件
    
    前两种   是会按照 yml文件方式展现
    第三种 会以json方式展现目录

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zjx
+ * @create 2020-05-13-14:15
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
