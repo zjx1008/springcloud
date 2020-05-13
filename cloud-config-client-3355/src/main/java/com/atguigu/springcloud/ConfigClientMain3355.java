89dd50549435a81fd5a00dc834da4ac6e397c9cc
zjx1008 <13141064938@163.com>
2020-05-13 17:05:00 +0800

    这个是 模拟 springcloud config 的客户端，3355获取3344 读取git 上的yml文件 第一步是加载pom的maven坐标  spring-cloud-start-config
    第二步时创建yml文件
     但是不是创建application.yml  而是bootstrap.yml
     第三步在配置文件中 配置 config 的客户端信息 来连接3344
    
     第四步配置controller 获取信息即可

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zjx
+ * @create 2020-05-13-16:08
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zjx
+ * @create 2020-05-13-16:09
+ */
+
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/configinfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
