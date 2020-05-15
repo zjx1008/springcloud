6395df9428eba14f0863d56edad0061dad596b36
zjx1008 <13141064938@163.com>
2020-05-15 14:28:52 +0800

    创建和3355 一模一样的项目

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zjx
+ * @create 2020-05-15-13:27
+ */
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
