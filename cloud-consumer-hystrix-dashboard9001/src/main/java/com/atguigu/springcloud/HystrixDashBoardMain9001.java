fff1bd3a33e72a707ec4ccd7f6f57d774c6e2189
zjx1008 <13141064938@163.com>
2020-05-25 17:23:49 +0800

    设置hstrix 的dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashBoardMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zjx
+ * @create 2020-05-11-17:49
+ */
+
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashBoardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashBoardMain9001.class,args);
+    }
+
+}
